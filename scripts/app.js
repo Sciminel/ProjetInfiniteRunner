39f3a9888f1434de28732a197f4fdf15a2453c94
Barkaoui Abdelkrim <53445220+Sciminel@users.noreply.github.com>
2022-02-07 18:07:42 +0100

    Mise en place de la detection de colision

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,8 @@
 
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext("2d")
+const scoreTotal = document.querySelector('.score');
+const paragraphe = document.querySelector('.para');
 
 canvas.width = '500';
 canvas.height = '300';
@@ -10,9 +12,13 @@ let presetTime = 1000;
 //Augmente la vitesse des ennemies a chaque intervall de 10 pts
 let ennemiSpeed = 5;
 
+//Lance ou arrete le jeu lorqu'il y a collision
+let game = true;
+let score = 0;
+
 //fonction qui genere un nombre random
 function getRandomNumbers(pMin,pMax){
-    return Math.floor(Math.random() * (max - min + 1)) + min ;
+    return Math.floor(Math.random() * (pMax - pMin + 1)) + pMin ;
 }
 
 function randomNumberInterval(timeInterval){
@@ -69,10 +75,10 @@ class Perso{
 }
 
 
-class Ennemi {
+class Ennemi{
     constructor(pSize, pSpeed){
         this.x = canvas.width + pSize;
-        this.y = canvas.height - pSize;
+        this.y = 300 - pSize;
         this.size = pSize;
         this.speed = pSpeed;
         this.color = 'red'
@@ -85,38 +91,78 @@ class Ennemi {
     slide(){
         //On appelle a chaque fois pour dessiner l'ennemi a chacun de ses déplacements
         this.draw();
-        this.x += this.speed;
+        this.x -= this.speed;
     }
 }
 
+let perso = new Perso(50,canvas.height - 50,50);
 
 
-let tabEnnemi = [];
+let tabEnnemis = [];
 // Auto generation d'ennemi dans un tableau 
 function generateEnnemi(){
     let timeDelay = randomNumberInterval(presetTime);
-    tabEnnemi.push(new Ennemi(50, ennemiSpeed));
+    tabEnnemis.push(new Ennemi(50, ennemiSpeed));
 
     setTimeout(generateEnnemi, timeDelay)
 }
 
-let perso = new Perso(50,canvas.height - 50,50,50);
+function detectionCollision(pX1, pY1, pX2, pY2){
+    if((pX1 + perso.size > pX2 && pY1 + perso.size > pY2) && (pX1 < pX2 + perso.size && pY1 + perso.size > pY2)){
+        return game = false;
+    }else {
+        return true;
+    }
+}
+
+
+function animate() {
+
+    if(game){
+        requestAnimationFrame(animate);
+        ctx.clearRect(0,0,canvas.width, canvas.height);
+    
+        //Dessine le perso
+        perso.draw();
+        tabEnnemis.forEach((element, index) => {
+            element.slide();
+            // Supprimer les element quand ils ont dépassés le coté gauche
+            if((element.x + element.size) <= 0){
+                setTimeout(() => {
+                    tabEnnemis.splice(index, 1)
+                }, 0)
+                score++;
+
+            }
+            
+            detectionCollision(perso.x, perso.y, element.x, element.y);
+        })
+        
+        scoreTotal.innerText = ` Score : ${score}`;
+        
+    }
+    if(!game){
+
+        if(score < 10){
+            scoreTotal.innerHTML = ` Dommage.. <br> Tu as fait un score de ${score}`;
+
+        }else if(score > 9 && score < 30){
+            scoreTotal.innerHTML = ` Bien joué.. mais peut mieux faire ;)  <br> Tu as fait un score de ${score}`;
+
+        }else {
+            scoreTotal.innerHTML = ` Trop fort pour moi... <br> Tu as fait un score de ${score}`;
+
+        }
+        paragraphe.innerHTML = `Pour refaire une partie appuyer sur Entrée`;
+    }
+
+
+}
 animate();
 setTimeout(() => {
     generateEnnemi();
 }, randomNumberInterval(presetTime));
 
-
-function animate() {
-
-    // requestAnimationFrame(animate);
-    ctx.clearRect(0,0,canvas.width, canvas.height);
-
-    //Dessine le perso
-    perso.draw();
-
-
-}
 
 
 //A l'ecoute de l'event de la touche jump
@@ -126,5 +172,12 @@ addEventListener('keydown', e => {
             perso.countJump = 0;
             perso.shouldJump = true;
         }
+    }
+
+    if(e.code === "Enter"){
+        score = 0;
+        game = true;
+        document.location.reload();
+        
     }
 })
